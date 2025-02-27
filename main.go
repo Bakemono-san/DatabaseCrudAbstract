5baa2bd91270704a8aee62fd729778f0ac6b8c3c
Mouhamadou moustapha diagne <mousdef34@gmail.com>
2025-02-27 16:00:45 +0000

    exporting the entity inside the struct

--- main.go
+++ main.go
@@ -4,27 +4,27 @@ import "fmt"
 
 // generic type
 type JpaRepository struct {
-	entity interface{}
+	Entity interface{}
 }
 
 // method to create a new record in the database
 func (r *JpaRepository) Create() {
-	fmt.Println("Create ", r.entity)
+	fmt.Println("Create ", r.Entity)
 }
 
 // method to update an existing record in the database
 func (r *JpaRepository) Update() {
-	fmt.Println("Update ", r.entity)
+	fmt.Println("Update ", r.Entity)
 }
 
 // method to delete a record from the database
 func (r *JpaRepository) Delete() {
-	fmt.Println("Delete ", r.entity)
+	fmt.Println("Delete ", r.Entity)
 }
 
 // method to find all records in the database
 func (r *JpaRepository) FindAll() {
-	fmt.Println("Find All ", r.entity)
+	fmt.Println("Find All ", r.Entity)
 }
 
 // method to find a record by ID in the database
